4e192a71f0a4d445a0dd7ff092ebb5f3174b9525
lishagao <gls2019gls@163.com>
2019-07-26 11:54:53 +0800

    写了vdetail

--- src/components/Vdetail.vue
+++ src/components/Vdetail.vue
@@ -4,6 +4,12 @@
             <span>&#xe512;</span>
             <span>菜谱详情</span>
         </nav>
+        <div id="detail">
+            <img class="firstpic" src="http://app2.showapi.com/img/caipuImg3/201611110205/1478801151775155_smallImg.jpg" alt="">
+            <div class="item">
+                <p class="title">爆浆鸡蛋</p>
+            </div>
+        </div>
 
     </div>
 </template>
@@ -17,22 +23,40 @@ export default {
 <style lang='less' scoped>
 @import url(../maincolor/maincolor.less);
 #vdetail{
-    margin-top:100px;
-    background: aqua;
+    margin-top:50px;
+  
         .navbar{
             font-family: 'myFont';
             font-size: 24px;
             width: 100%;
             height: 50px;
             line-height: 50px;
-            text-align: center;
+            // text-align: center;
             background-color:@mainColor;
             position:fixed;
             top:0;
             color:white;
-            display:flex;
-            justify-content: space-around;
+            span:nth-of-type(1){
+                margin-right: 100px;
+                margin-left: 10px;
+            }
         }
+       
     
    }
+    #detail{
+                width:100%;
+                height:200px;
+                background-color: darkcyan;
+            .firstpic{
+                        width:100%;
+                        height:200px;
+                }
+                .item{
+                    padding: 0 10px;
+                    .title{
+                        font-size: 20px;
+                    }
+                }
+        }
 </style>
