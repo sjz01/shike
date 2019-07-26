e7e30e2aa76d257f7ec6f0db7205771334bb1c47
Wonderland-lx <wonderland_lx@163.com>
2019-07-26 15:07:59 +0800

    修改了classify

--- src/store/liux.js
+++ src/store/liux.js
@@ -5,11 +5,14 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    myTitle:"食客",
   },
   mutations: {
-
-  },
+    //修改state中的属性值
+    // changeMyTitle:(state,payload)=>{
+    //   state.myTitle = payload.title;
+    // }
+    },
   actions: {
 
   }
