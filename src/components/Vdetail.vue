326cf9f2863e38d4fdbd8ba10b178016ce50e86d
lishagao <gls2019gls@163.com>
2019-07-26 15:01:33 +0800

    vdetail

--- src/components/Vdetail.vue
+++ src/components/Vdetail.vue
@@ -8,9 +8,9 @@
             <img class="firstpic" src="http://app2.showapi.com/img/caipuImg3/201611110205/1478801151775155_smallImg.jpg" alt="">
             <div class="item">
                 <p class="title">爆浆鸡蛋</p>
-                <P>生活里真的是创意无限,虽然都是一样的柴米油盐,有些家吃的欢,有些家吃的愁。所以要吃的好，不在于你买了什么，而是你即使用简单的材料也能做出让家人有惊喜的美食。今天这道美食就是这样，简单的食材，只用到了一个鸡蛋和一张海苔就能让你尝到从没吃过的味道，不用加一点调料，不用盐，不用味精！</P>
-                <P>材料</P>
-                <ul>
+                <P class="des">生活里真的是创意无限,虽然都是一样的柴米油盐,有些家吃的欢,有些家吃的愁。所以要吃的好，不在于你买了什么，而是你即使用简单的材料也能做出让家人有惊喜的美食。今天这道美食就是这样，简单的食材，只用到了一个鸡蛋和一张海苔就能让你尝到从没吃过的味道，不用加一点调料，不用盐，不用味精！</P>
+                <P class="food">材料</P>
+                <ul class="foods">
                     <li>鸡蛋</li>
                     <li>海苔</li>
                 </ul>
@@ -37,6 +37,7 @@
                 <p>小贴士</p>
                 <p>1. 海苔四周抹水是因为放入鸡蛋后可以把海苔包拢，鸡蛋不会流出； 2. 如果有的海苔太薄，建议用两张比较安全； 3. 煎之前油要烧热再放入才会炸的快，大约炸一分钟左右就可以了。 4. 用平底锅也可以少放油，就是把平底锅倾斜一下油就集中到一边了。</p>
             </div>
+            <div class="collect">收藏菜谱</div>
         </div>
 
     </div>
@@ -52,7 +53,7 @@ export default {
 @import url(../maincolor/maincolor.less);
 #vdetail{
     margin-top:50px;
-    margin-bottom: 60px;
+    padding:0 10px 0;
         .navbar{
             font-family: 'myFont';
             font-size: 24px;
@@ -86,5 +87,16 @@ export default {
                         font-size: 20px;
                     }
                 }
-        }
+                .collect{
+                  
+                    width: 120px;
+                    height: 40px;
+                    line-height: 40px;
+                    text-align: center;
+                    margin: 0 auto;
+                    border-radius: 5px;
+                    background-color: tomato;
+                    margin-bottom: 60px;
+                }
+   }
 </style>
