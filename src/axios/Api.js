8a2e4a2e329690605204d8d32e4e2933e6fa1419
（对应你github的用户名） <github>
2019-07-29 16:45:10 +0800

    Api.js修改密钥

--- src/axios/Api.js
+++ src/axios/Api.js
@@ -16,8 +16,8 @@ function myPost(vue){
 }
 function myPostde(vue){
     var param = new URLSearchParams();
-    param.append('showapi_appid', "100960");
-    param.append("showapi_sign", "7eb84dcfcebe47f09a88af220a551b7a");
+    param.append('showapi_appid', "100678");
+    param.append("showapi_sign", "a3fc1e6d5e9140ff99d86ec74d30fc44");
     param.append("type",'蛋类');
     param.append('id','5c3dfddfe9b6cc65afc9427e')
     return vue.axios.post(imgpath,param);
