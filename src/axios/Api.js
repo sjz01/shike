ac715a69ed83125d691a8a7c7526d66440795530
Wonderland-lx <wonderland_lx@163.com>
2019-07-29 20:15:30 +0800

    axios

--- src/axios/Api.js
+++ src/axios/Api.js
@@ -16,8 +16,8 @@ function myPost(vue){
 }
 function myPostde(vue){
     var param = new URLSearchParams();
-    param.append('showapi_appid', "100678");
-    param.append("showapi_sign", "a3fc1e6d5e9140ff99d86ec74d30fc44");
+    param.append('showapi_appid', "101105");
+    param.append("showapi_sign", "61383dfe18e6445c92815d23a219b39b");
     param.append("type",'蛋类');
     param.append('id','5c3dfddfe9b6cc65afc9427e')
     return vue.axios.post(imgpath,param);
