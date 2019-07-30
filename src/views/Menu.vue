942145123ca2eca9dd72d332bd3616e9ceea8a0a
（对应你github的用户名） <github>
2019-07-30 16:14:18 +0800

    提交Menu.vue修改

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -1,78 +1,86 @@
 <template>
     <div id="menu">
         <router-link to="/vdetail">
-            <div class="vtl_d">
-                <img class="pic" src="http://www.talkimages.cn/images/medium/20133071/tkf003_535296.jpg" alt="">
-                <p class="pic_font">减肥餐</p>
+            <div class="vtl_d" v-for="(item,key) in $store.state.mingyu.food" :key = "key" @click="chuan(item)">
+                <img class="pic" :src="item.smallImg" alt="图片加载异常">
+                <p class="pic_font">{{item.cpName}}</p>
             </div>
-            <div class="vtl_d">
-                <img class="pic" src="http://www.talkimages.cn/images/medium/20133071/tkf003_535296.jpg" alt="">
-                <p class="pic_font">减肥餐</p>
-            </div>
-            <div class="vtl_d">
-                <img class="pic" src="http://www.talkimages.cn/images/medium/20133071/tkf003_535296.jpg" alt="">
-                <p class="pic_font">减肥餐</p>
-            </div>
-            <div class="vtl_d">
-                <img class="pic" src="http://www.talkimages.cn/images/medium/20133071/tkf003_535296.jpg" alt="">
-                <p class="pic_font">减肥餐</p>
-            </div>
-            <div class="vtl_d">
-                <img class="pic" src="http://www.talkimages.cn/images/medium/20133071/tkf003_535296.jpg" alt="">
-                <p class="pic_font">减肥餐</p>
-            </div>
-            <div class="vtl_d">
-                <img class="pic" src="http://www.talkimages.cn/images/medium/20133071/tkf003_535296.jpg" alt="">
-                <p class="pic_font">减肥餐</p>
-            </div>
-            <div class="vtl_d">
-                <img class="pic" src="http://www.talkimages.cn/images/medium/20133071/tkf003_535296.jpg" alt="">
-                <p class="pic_font">减肥餐</p>
-            </div>
-            <p class="vtl_p">亲，到底啦~</p>
         </router-link>
+            <p class="vtl_p">亲，到底啦~</p>
     </div>
 
 
 </template>
 
 <script>
+import http from "@/axios/Api.js";
 export default {
-    name:"Menu"
-}
+  name: "Menu",
+  data() {
+    return {
+      cpName: "",
+      smallImg: "",
+      food: []
+    };
+  },
+  methods: {
+    getdata() {
+
+      http.getDetail(this,'早餐').then(res => {
+        // res.data.showapi_res_body.datas.forEach((item)=>{
+        // this.$store.state.mingyu.cpName.push(item.cpName);
+        // this.$store.state.mingyu.smallImg.push(item.smallImg);
+        // });
+        this.$store.state.mingyu.food = res.data.showapi_res_body.datas;
+        // console.log(this.$store.state.mingyu.food)
+      });
+      // console.log(this.$store.state.mingyu.smallImg)
+    },
+    chuan(item) {
+        //  console.log(item)
+        //  console.log(item.id)
+        this.$store.state.id = item.id;
+        this.$store.state.type = item.type_v2;
+        // console.log(  this.$store.state.id)
+        // console.log( this.$store.state.type)
+    }
+  },
+  created() {
+    this.getdata();
+  }
+};
 </script>
 
 <style lang='less' scoped>
-   #menu{
-       margin-top: 50px;
-       color: black;
+#menu {
+  margin-top: 50px;
+  color: black;
 
-       .vtl_d {
-           width: 94%;
-           height: 280px;
-           margin: 0 auto;
-           margin-top: 10px;
-           border-bottom:1px solid #ccc;
-           
+  .vtl_d {
+    width: 94%;
+    height: 280px;
+    margin: 0 auto;
+    margin-top: 10px;
+    border-bottom: 1px solid #ccc;
 
-           .pic {
-               width: 100%;
-               margin-top: 6px;
-           }
-           .pic_font{
-                   margin: 4px 0;
-                   font-size: 18px;
-                   font-weight: 600;
-                   color: black ;
-                   text-decoration: none;
-            }
-       }
-    .vtl_p {
-        height: 30;
-        line-height: 30px;
-        text-align: center;
-        color: black;
-        margin-bottom: 70px;
+    .pic {
+      width: 100%;
+      margin-top: 6px;
     }
-   }
+    .pic_font {
+      margin: 4px 0;
+      font-size: 18px;
+      font-weight: 600;
+      color: black;
+      text-decoration: none;
+    }
+  }
+  .vtl_p {
+    height: 30;
+    line-height: 30px;
+    text-align: center;
+    color: black;
+    margin-bottom: 70px;
+  }
+}
 </style>
