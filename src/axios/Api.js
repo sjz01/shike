bedc423260fd5e8806e6e7e30f3b33e80d229036
lishagao <gls2019gls@163.com>
2019-07-30 11:16:54 +0800

    修改

--- src/axios/Api.js
+++ src/axios/Api.js
@@ -1,6 +1,6 @@
 //接口的配置
- const showapi_appid = '100960'
- const showapi_sign = '7eb84dcfcebe47f09a88af220a551b7a'
+ const showapi_appid = '101105'
+ const showapi_sign = '61383dfe18e6445c92815d23a219b39b'
  const classpath = 'http://route.showapi.com/1164-2'
  const imgpath = 'http://route.showapi.com/1164-1'
 //配置路径
@@ -10,12 +10,9 @@
 // post方法总入口
 function myPost(vue){
     var param = new URLSearchParams();
-    param.append('showapi_appid', "101064");
-    param.append("showapi_sign", "aac7679ea2754daab3c11ee13a135a37");
+    param.append('showapi_appid',showapi_appid);
+    param.append("showapi_sign", showapi_sign);
     return vue.axios.post(classpath,param);
-}
-function myPostde(vue){
-
 }
 function getcmenu(vue) {
     return myPost(vue,classpath,{});
@@ -24,12 +21,22 @@ function getDetail(vue,type){
     // var id = id;
     var type = type;
     var param = new URLSearchParams();
-    param.append('showapi_appid', "101064");
-    param.append("showapi_sign", "aac7679ea2754daab3c11ee13a135a37");
+    param.append('showapi_appid', showapi_appid);
+    param.append("showapi_sign", showapi_sign);
     param.append('type', type);
     // param.append("id", id);
     return vue.axios.post(imgpath,param);
 }
+function getVdetail (vue,type,id){
+    var id = id;
+    var type = type;
+    var param = new URLSearchParams();
+    param.append('showapi_appid', showapi_appid);
+    param.append("showapi_sign", showapi_sign);
+    param.append('type', type);
+    param.append("id", id);
+    return vue.axios.post(imgpath,param);
+}
 export default {
-    getcmenu,getDetail
+    getcmenu,getDetail,getVdetail
 }
