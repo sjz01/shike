c2cd8561891c854f71c471f780d77fe7ed3b7054
Wonderland-lx <wonderland_lx@163.com>
2019-07-30 10:27:37 +0800

    cmenu

--- src/components/Cmenu.vue
+++ src/components/Cmenu.vue
@@ -5,12 +5,13 @@
              <span> 食客 </span>
          </nav>
          <router-link tag=ul to="/vdetail">
-             <li>
+             <li v-for="(item,key) in $store.state.liux.des" :key='key'>
                  <div id="imgk">
-                     <img src="http://app2.showapi.com/img/caipuImg3/201611110205/1478801151775155_smallImg.jpg" alt="">
+                     <img :src="item.smallImg" alt="">
                  </div>
                    <div id="des">
-                 "生活里真的是创意无限,虽然都是一样的柴米油盐,有些家吃的欢,有些家吃的愁。"
+                    <p>{{item.cpName}}</p>
+                    <p>{{item.des}}</p>
                   </div>
              </li>
          
@@ -19,77 +20,83 @@
 </template>
 
 <script>
-
 //按需引入
-import http from '@/axios/Api.js'
+import http from "@/axios/Api.js";
 
 export default {
-   
-  name:"Cmenu",
-  data(){
-      return{
-        //  showapi_appid: showapi_appid,
-        //  showapi_sign:showapi_sign
-      }
+  name: "Cmenu",
+  data() {
+    return {
+      cpname: "",
+      des: "",
+      img: ""
+    };
   },
   methods: {
-      back(){
-          this.$router.go(-1)
-      },
-      getdata(){
-           http.getDetail(this,this.$store.liux.right[0])
-           .then((res) => {
-               console.log(res);
-            })
-     
-     },
-  
+    back() {
+      this.$router.go(-1);
     },
-    created() {
-        this.getdata()
-    },
-}
+    getdata() {
+      var a = this.$store.state.liux.right[0];
+      console.log(a);
+      http.getDetail(this, a).then(res => {
+        console.log(res.data.showapi_res_body.datas);
+        //    this.$store.state.liux.type.push(res.data.showapi_res_body.datas[0].id)
+
+        this.$store.state.liux.des = res.data.showapi_res_body.datas;
+      });
+      console.log();
+    }
+  },
+  created() {
+    this.getdata();
+  }
+};
 </script>
 
 <style lang='less' scoped>
 @import url(../maincolor/maincolor.less);
-#cmenu{
-   width: 100%;
+#cmenu {
+  width: 100%;
 
+  #navbar {
+    font-family: "myFont";
+    width: 80%;
+    height: 50px;
+    background: @mainColor;
+    position: fixed;
+    top: 0;
+    color: white;
+    text-align: center;
+    line-height: 50px;
+    font-size: 24px;
+    margin-left: 20px;
+    z-index: 12;
 
-    #navbar{
-         font-family: 'myFont';
-        width:80%;
-         height:50px;
-        background: @mainColor;
-         position: fixed;
-         top: 0;
-         color:white;
-         text-align: center;
-         line-height: 50px;
-        font-size: 24px;
-        margin-left: 20px;
-        z-index: 12;
-
-         span:nth-of-type(1){
-           float: left;
-         }
+    span:nth-of-type(1) {
+      float: left;
     }
+  }
 
-    ul{
-        width:90%;
-        margin:0 auto;
-        margin-top: 54px;
+  ul {
+    width: 90%;
+    margin: 0 auto;
+    margin-top: 54px;
 
-        li{
-            display: flex;
+    li {
+      display: flex;
 
-            div{
-                margin:5px;
-            }
-        }
+      div {
+        margin: 5px;
+      }
+
+      #des{
+          width:50%;
+          height:50px;
+      }
     }
+  }
 
-    margin-bottom:62px; 
-   }
+  margin-bottom: 62px;
+}
 </style>
--- src/store/liux.js
+++ src/store/liux.js
@@ -1,5 +1,10 @@
 export default{
   name:'刘笑',
   leftList:[],
-  right:[]
+  right:[],
+  id:[],
+  cpName:[],
+  smallImg:[],
+  des:[]
+ 
 }
