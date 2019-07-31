183e2a3d7947ba2344a73a18aeb4074f9dd3994c
（对应你github的用户名） <github>
2019-07-31 11:39:28 +0800

    首页修改完成

--- src/components/Hots6.vue
+++ src/components/Hots6.vue
@@ -1,5 +1,6 @@
 <template>
     <div id="menu">
+        <span class="left" @click="back">&#xe512;</span>
         <router-link to="/vdetail">
             <div class="vtl_d" v-for="(item,key) in $store.state.mingyu.food" :key = "key" @click="chuan(item)">
                 <img class="pic" :src="item.smallImg" alt="图片加载异常">
@@ -24,9 +25,12 @@ export default {
     };
   },
   methods: {
+     back(){
+      this.$router.go(-1);
+    },
     getdata() {
         
-      http.getDetail(this,'早餐').then(res => {
+      http.getDetail(this,'三明治').then(res => {
         // res.data.showapi_res_body.datas.forEach((item)=>{
         // this.$store.state.mingyu.cpName.push(item.cpName);
         // this.$store.state.mingyu.smallImg.push(item.smallImg);
@@ -52,9 +56,25 @@ export default {
 </script>
 
 <style lang='less' scoped>
+@import url(../maincolor/maincolor.less);
+
 #menu {
   margin-top: 50px;
   color: black;
+
+  .left {
+    font-family: "myFont";
+    font-size: 24px;
+    width: 14%;
+    height: 50px;
+    line-height: 50px;
+    text-align: center;
+    background-color: @mainColor;
+    position:fixed;
+    top:0;
+    color: white;
+    z-index: 11;
+  }
 
   .vtl_d {
     width: 94%;
@@ -68,12 +88,13 @@ export default {
       margin-top: 6px;
     }
     .pic_font {
-    margin: 4px 0;
-    font-size: 16px;
+      margin: 4px 0;
+     font-size: 16px;
     height: 22px;
     line-height: 22px;
-    font-weight: 600;
-    color: black;
+     font-weight: 600;
+     color: black;
+     text-decoration: none;
     }
   }
   .vtl_p {
